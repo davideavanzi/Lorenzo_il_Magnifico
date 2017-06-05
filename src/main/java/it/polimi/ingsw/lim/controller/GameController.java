544e9eee731035d4e8be0922d78263dc329b8d0f
davideavanzi <ava@davideavanzi.com>
2017-06-05 12:36:08 +0200

    Players are now automatically assigned a color in instantiation. Fixed bug in strength creation. More bugfixing from sonar.

--- src/main/java/it/polimi/ingsw/lim/controller/GameController.java
+++ src/main/java/it/polimi/ingsw/lim/controller/GameController.java
@@ -6,6 +6,7 @@ import it.polimi.ingsw.lim.model.Game;
 import it.polimi.ingsw.lim.parser.Parser;
 
 import java.util.ArrayList;
+import java.util.logging.Level;
 
 import static it.polimi.ingsw.lim.Log.*;
 import static it.polimi.ingsw.lim.Settings.*;
@@ -24,7 +25,7 @@ public class GameController {
         getLog().info("Creating new game instance.");
         Game game = new Game();
         String defaultPath = "default/";
-        getLog().info("Parsing game data from path: "+CONFIGS_PATH+defaultPath);
+        getLog().log(Level.INFO, "Parsing game data from path: $s", CONFIGS_PATH+defaultPath);
 
         //TODO: handle exception in a proper place
         Parser parsedGame = new Parser();
@@ -36,14 +37,16 @@ public class GameController {
 
         getLog().info("Validating game data with current settings.");
         //TODO: how to call validating method?
+        if(validateParsedData(parsedGame))
+            getLog().severe("[ERROR] - parsed data are incorrect");
 
 
         //building game
         getLog().info("Setting up game with parsed data");
         //TODO: add players before setting up the game!;
-        game.addPlayer("CIAONE", GREEN_COLOR);
-        game.addPlayer("HELLONE", YELLOW_COLOR);
-        game.addPlayer("HOLAONE", BLUE_COLOR);
+        game.addPlayer("CIAONE");
+        game.addPlayer("HELLONE");
+        game.addPlayer("HOLAONE");
         try {
             game.setUpGame(parsedGame);
         } catch (GameSetupException e) {
@@ -65,11 +68,16 @@ public class GameController {
      * - Council bonuses consistent.
      * - More controls?
      * - Faith track bonuses corresponding to faith track length
-     * do we have to check if every card is valid?
+     * TODO: do we have to check if every card is valid?
      */
 
-    private static boolean validateParsedData(){
-        //TODO: implement here
+    private static boolean validateParsedData (Parser parsedGame){
+        for (int age = 1; age <= AGES_NUMBER; age++)
+            for (String color : DEFAULT_TOWERS_COLORS)
+                return !(parsedGame.getCard(age, color).size() == TURNS_PER_AGE * TOWER_HEIGHT);
+        for (String color : DEFAULT_TOWERS_COLORS)
+            return !(parsedGame.getTowerbonuses(color).length == TOWER_HEIGHT);
+        //TODO: implement
         return true;
     }
 
