bc81fa590e2808d242e411d9d0b440e63dfb8e83
FabCars95 <10492934@polimi.it>
2017-06-23 10:26:09 +0200

    Add configurator timer

---
+++ src/main/java/it/polimi/ingsw/lim/exceptions/InvalidTimerException.java
@@ -0,0 +1,10 @@
+package it.polimi.ingsw.lim.exceptions;
+
+/**
+ * Created by fabcars
+ */
+public class InvalidTimerException extends Exception{
+    public InvalidTimerException (String errorMessage){
+        super(errorMessage);
+    }
+}
