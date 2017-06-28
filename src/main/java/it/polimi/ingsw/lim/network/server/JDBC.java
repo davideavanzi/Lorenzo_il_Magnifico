a6cb8b835f1eca7582d3620fcb69382ac98ce2ee
FabCars95 <10492934@polimi.it>
2017-06-28 14:57:17 +0200

    added jdbc databse

--- src/main/java/it/polimi/ingsw/lim/network/server/JDBC.java
+++ src/main/java/it/polimi/ingsw/lim/network/server/JDBC.java
@@ -1,5 +1,7 @@
 package it.polimi.ingsw.lim.network.server;
 
+import it.polimi.ingsw.lim.Log;
+
 import java.sql.*;
 
 /**
@@ -21,22 +23,34 @@ public class JDBC {
         this.statement = this.connection.createStatement();
     }
 
-    public void createDataBase() throws SQLException, ClassNotFoundException{
-        String sql = "CREATE DATABASE USERS";
-        this.statement.executeUpdate(sql);
-        DB_URL = "jdbc:mysql://127.0.0.1:3306/".concat("USERS");
-        this.connection = DriverManager.getConnection(DB_URL, USER, PASS);
-        this.statement = this.connection.createStatement();
+    public void createDataBase() {
+        try {
+            String sql = "CREATE DATABASE USERS";
+            this.statement.executeUpdate(sql);
+            DB_URL = DB_URL.concat("USERS");
+        }
+        catch (SQLException e){
+            DB_URL = DB_URL.concat("USERS");
+            Log.getLog().info("databse already exists");
+        }
     }
 
-    public void createTable() throws SQLException{
-        String sql = "CREATE TABLE REGISTRATION ".concat("(userName VARCHAR(255) not NULL, ").concat(" password VARCHAR(255) not NULL, ").concat(" PRIMARY KEY ( userName ))");
-        this.statement.executeUpdate(sql);
+    public void createTable() {
+        try {
+            this.connection = DriverManager.getConnection(DB_URL, USER, PASS);
+            this.statement = this.connection.createStatement();
+            String sql = "CREATE TABLE REGISTRATION ".concat("(userName VARCHAR(255) not NULL, ").concat(" password VARCHAR(255) not NULL, ").concat(" PRIMARY KEY ( userName ))");
+            this.statement.executeUpdate(sql);
+        }
+        catch (SQLException e){
+            Log.getLog().info("table already exists");
+        }
     }
 
     public void insertRecord(String userName, String password) throws SQLException{
         String sql = "INSERT INTO REGISTRATION VALUES ('".concat(userName).concat("', '").concat(password.concat("')"));
         this.statement.executeUpdate(sql);
+        Log.getLog().info("user: ".concat(userName).concat(" added"));
     }
 
     public boolean isContain (String userName, String password) throws SQLException{
@@ -44,12 +58,16 @@ public class JDBC {
         ResultSet resultSet = statement.executeQuery(sql);
         while (resultSet.next()){
             if (resultSet.getString("userName").equals(userName)){
+                Log.getLog().info("user: ".concat(userName).concat(" exist"));
                 if (resultSet.getString("password").equals(password)){
+                    Log.getLog().info("password correct");
                     return true;
                 }
+                Log.getLog().info("password wrong");
             }
         }
         resultSet.close();
+        Log.getLog().info("user: ".concat(userName).concat(" not exist"));
         return false;
     }
 
@@ -58,6 +76,7 @@ public class JDBC {
         ResultSet resultSet = statement.executeQuery(sql);
         while (resultSet.next()){
             if (resultSet.getString("userName").equals(userName)){
+                Log.getLog().info("user: ".concat(userName).concat(" already exists"));
                 return true;
             }
         }
@@ -66,34 +85,13 @@ public class JDBC {
     }
 
     public void deleteUser(String userName) throws SQLException{
-        String sql = "DELETE FROM Registration ".concat("WHERE userName = '").concat(userName).concat("'");
+        String sql = "DELETE FROM REGISTRATION ".concat("WHERE userName = '").concat(userName).concat("'");
         statement.executeUpdate(sql);
+        Log.getLog().info("user: ".concat(userName).concat(" deleted"));
     }
 
     private void dropDataBase() throws SQLException{
         String sql = "DROP DATABASE USERS";
         statement.executeUpdate(sql);
-    }
-
-    public boolean ifExistTable() throws SQLException{
-        DatabaseMetaData meta = this.connection.getMetaData();
-        ResultSet res = meta.getTables(null, null, "REGISTRATION", null);
-        return res.next();
-    }
-
-    public static void main(String args[]){
-        try {
-            JDBC jdbc = new JDBC();
-            jdbc.createDataBase();
-            jdbc.createTable();
-            jdbc.insertRecord("user", "pass");
-            System.out.println(jdbc.isContain("user", "pass"));
-            System.out.println(jdbc.isContain("asd", "zxc"));
-            jdbc.ifExistTable();
-            jdbc.dropDataBase();
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
     }
 }
