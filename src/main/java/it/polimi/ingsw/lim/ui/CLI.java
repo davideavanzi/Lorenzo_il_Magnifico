42ae7cfc09d095a9f86fbf10790926691dcba52e
FabCars95 <10492934@polimi.it>
2017-07-07 19:30:28 +0200

    minor chages

--- src/main/java/it/polimi/ingsw/lim/ui/CLI.java
+++ src/main/java/it/polimi/ingsw/lim/ui/CLI.java
@@ -57,7 +57,59 @@ public class CLI extends AbsUI {
     }
 
     private void askForFastTowerMove() {
-
+        ArrayList<String> tower = new ArrayList<>();
+        printGameMessageln("You can activate a fast tower move!");
+        printMessageln("How many Servants would you like to deploy to support your move?");
+        do {
+            inputNum = userInput.nextInt();
+        } while (inputNum < 0);
+        int servantsDeployed = inputNum;
+        printMessageln("In which tower would you like to pick a Card?");
+        tower.add("1) Green Tower");
+        tower.add("2) Yellow Tower");
+        tower.add("3) Blue Tower");
+        tower.add("4) Purple Tower");
+        if(uiCallback.getLocalPlayers().size() == 5){
+            tower.add("5) Black Tower");
+        }
+        for(String color: tower){
+            printMessageln(color);
+        }
+        if(uiCallback.getLocalPlayers().size() < 5) {
+            do {
+                inputNum = userInput.nextInt();
+            } while (inputNum < 0 || inputNum > 5);
+        }
+        if(uiCallback.getLocalPlayers().size() == 5) {
+            do {
+                inputNum = userInput.nextInt();
+            } while (inputNum < 0 || inputNum > 6);
+        }
+        int chosenTower = inputNum;
+        String towerColor = null;
+        switch (chosenTower){
+            case 1:
+                towerColor = "GREEN";
+                break;
+            case 2:
+                towerColor = "BLUE";
+                break;
+            case 3:
+                towerColor = "YELLOW";
+                break;
+            case 4:
+                towerColor = "PURPLE";
+                break;
+            case 5:
+                towerColor = "BLACK";
+                break;
+        }
+        printMessageln("In which floow of ".concat(towerColor.toLowerCase()).concat(" you would you like to pick a card?"));
+        do {
+            inputNum = userInput.nextInt();
+        } while (inputNum < 0 || inputNum > TOWER_HEIGHT);
+        int floor = inputNum;
+        uiCallback.sendFastTowerMove(servantsDeployed, towerColor, floor);
     }
 
     private void askForProductionOptions() {
